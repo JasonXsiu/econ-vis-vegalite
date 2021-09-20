a898fd9d733de3ac1f3e6bef1f0695919909f9d3
JasonXsiu <sjsa3a28@gmail.com>
2021-09-20 12:42:49 +1000

    first time

---
+++ vega_lite_vis.js
@@ -0,0 +1,31 @@
+var vg_eg = "econ_growth.json";
+var vg_trade = "trade.json";
+var vg_investment = "investment.json";
+var vg_sector_gdp = "sector_gdp.json";
+var vg_pvty_map = "pvty_map.json";
+var vg_trade_cop = "trade copy.json";
+
+
+vegaEmbed("#econ_growth", vg_eg).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#trade", vg_trade).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#investment", vg_investment).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#sector_gdp", vg_sector_gdp).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#pvty_map", vg_pvty_map).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#trade_copy", vg_trade_cop).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
